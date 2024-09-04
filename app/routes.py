ef53a6769823d17bbc7c4f54ed27c09526bf9200
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2024-09-04 10:32:42 +0800

    bookshelf page done

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,12 @@
 from flask import render_template, current_app
+import time
+import math
+import pickle
+
+def load_dict():
+    global mapp
+    with open("./datas/mapp.pkl", "rb") as f:
+        mapp = pickle.load(f)
 
 def create_routes(app):
     @app.route('/')
@@ -15,19 +23,29 @@ def create_routes(app):
         
         return render_template('index.html', new_books=new_books, reviews=reviews, bookshelves=bookshelves)
 
-    @app.route('/bookshelf')
-    def bookshelf():
+    @app.route('/bookshelf/<page>')
+    def bookshelf(page):
         book_keys = current_app.redis.keys('book:*')
         books = []
-        for key in book_keys:
-            book = current_app.redis.hgetall(key)
-            book_details = {k: v for k, v in book.items()}
-            # added book_key in book_details
-            book_details['book_key'] = key
-            books.append(book_details)
-            print(key, book_details)
-        
-        return render_template('bookshelf.html', books=books)
+        page = int(page)
+        page_start = 1
+        page_end = math.ceil(len(book_keys)/24)
+        # showing from books[book_start] to books[book_end]
+        book_start = (page-1)*24
+        book_end = page*24
+        i = 0
+
+        for key in book_keys: 
+            if i>=book_start and i<book_end:
+                book = current_app.redis.hgetall(key)
+                book_details = {k: v for k, v in book.items()}
+                # added book_key in book_details
+                book_details['book_key'] = key
+                books.append(book_details)
+            i+=1
+            
+
+        return render_template('bookshelf.html', books=books, books_count =len(book_keys), page_current = page, page_start = page_start, page_end = page_end)
 
     @app.route('/book/<isbn>')
     def book_detail(isbn):
